bbf8d39a228da0e706ba43967393077e98cfdc87
Anteru <andre.shale@yandex.ru>
2023-08-15 22:10:10 +0300

    feat(resetpassword): a new ResetPassword component based on the Login component

--- src/utils/ValidationSchemes.js
+++ src/utils/ValidationSchemes.js
@@ -17,3 +17,14 @@ export const LoginSchema = yup.object().shape({
 		.max(30, ERROR_MESSAGES.PASSWORD.TO_LONG)
 		.required(ERROR_MESSAGES.PASSWORD.EMPTY),
 });
+
+export const ResetPassordSchema = yup.object().shape({
+	email: yup
+		.string()
+		.email(ERROR_MESSAGES.EMAIL.INCORRECT)
+		.required(ERROR_MESSAGES.EMAIL.EMPTY)
+		.test('Validate Email', ERROR_MESSAGES.EMAIL.INCORRECT, (value) => {
+			const RegEx = emailRegex;
+			return RegEx.test(String(value).toLowerCase());
+		}),
+});
