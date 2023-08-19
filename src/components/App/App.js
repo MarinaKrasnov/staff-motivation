b34f5ea42893a1fdfb47de0f498e9cf6471ab13d
Anna Haratova <black-isida@yandex.ru>
2023-08-19 19:15:07 +0300

    style(register): updated comments

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,17 @@
 import './App.css';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Register from '../Register/Register';
+import Register from '../Register/Register'; // Аня - страница регистрации
 
 function App() {
 	return (
-		// Аня - добавлен модуль регистрации
+		// Аня - добавлен модуль регистрации (начало кода)
 		<div className="App">
 			<Routes>
 				<Route path="/signup" element={<Register />} />
 			</Routes>
 		</div>
+		// Аня - добавлен модуль регистрации (конец кода)
 	);
 }
 
--- src/utils/ValidationSchemes.js
+++ src/utils/ValidationSchemes.js
@@ -2,33 +2,6 @@ import * as yup from 'yup';
 import emailRegex, { nameRegex } from './RegExps'; // Аня - страница регистрации
 import { ERROR_MESSAGES } from './Config';
 
-export const LoginSchema = yup.object().shape({
-	email: yup
-		.string()
-		.email(ERROR_MESSAGES.EMAIL.INCORRECT)
-		.required(ERROR_MESSAGES.EMAIL.EMPTY)
-		.test('Validate Email', ERROR_MESSAGES.EMAIL.INCORRECT, (value) => {
-			const RegEx = emailRegex;
-			return RegEx.test(String(value).toLowerCase()); // использую регулярное выражение для валидации е-мейла
-		}),
-	password: yup
-		.string()
-		.min(6, ERROR_MESSAGES.PASSWORD.TO_SHORT)
-		.max(30, ERROR_MESSAGES.PASSWORD.TO_LONG)
-		.required(ERROR_MESSAGES.PASSWORD.EMPTY),
-});
-
-export const ResetPassordSchema = yup.object().shape({
-	email: yup
-		.string()
-		.email(ERROR_MESSAGES.EMAIL.INCORRECT)
-		.required(ERROR_MESSAGES.EMAIL.EMPTY)
-		.test('Validate Email', ERROR_MESSAGES.EMAIL.INCORRECT, (value) => {
-			const RegEx = emailRegex;
-			return RegEx.test(String(value).toLowerCase());
-		}),
-});
-
 // Аня - валидация на странице регистрации(начало кода)
 export const RegisterSchema = yup.object().shape({
 	firstName: yup
