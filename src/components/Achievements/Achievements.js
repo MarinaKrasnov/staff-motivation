fa6ad947dc7584213b5f9f7473a82ee3e23205a6
Anteru <andre.shale@yandex.ru>
2023-08-29 01:01:40 +0300

    feat(achievements): new component for Main + main font

---
+++ src/components/Achievements/Achievements.js
@@ -0,0 +1,55 @@
+import './Achievements.scss';
+import achieveLogo from '../../images/achieve-mark.svg';
+
+export default function Achievements() {
+	const score = 30; // временно
+	const completion = 75; // временно
+	return (
+		<section className="achievements">
+			<h2 className="achievements__title">Достижения</h2>
+			<ul className="achievements__stats-container">
+				<li className="achievements__achieve">
+					<p className="achievements__name">Качество работы</p>
+					<div className="achievements__score-container">
+						<img
+							className="achievements__logo"
+							src={achieveLogo}
+							alt="достижение"
+						/>
+						<p className="achievements__score">{`+${score} Б`}</p>
+					</div>
+					<p className="achievements__completion">По итогам спринта</p>
+				</li>
+
+				<li className="achievements__achieve">
+					<p className="achievements__name">Вовлечённость</p>
+					<div className="achievements__score-container">
+						<img
+							className="achievements__logo"
+							src={achieveLogo}
+							alt="достижение"
+						/>
+						<p className="achievements__score">{`+${score} Б`}</p>
+					</div>
+					<p className="achievements__completion">По итогам спринта</p>
+				</li>
+
+				<li className="achievements__achieve">
+					<p className="achievements__name">Соблюдение дедлайна</p>
+					<div className="achievements__score-container">
+						<p className="achievements__score">{`+${score} Б`}</p>
+					</div>
+					<p className="achievements__completion">{`Выполнено на: ${completion}%`}</p>
+				</li>
+
+				<li className="achievements__achieve">
+					<p className="achievements__name">Пройдено обучение</p>
+					<div className="achievements__score-container">
+						<p className="achievements__score">{`+${score} Б`}</p>
+					</div>
+					<p className="achievements__completion">{`Выполнено на: ${completion}%`}</p>
+				</li>
+			</ul>
+		</section>
+	);
+}
